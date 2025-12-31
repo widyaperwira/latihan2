c4620f6308946fd043e8f4f5857857e3b462f236
Widya Perwira Utama <widyaperwira@gmail.com>
2025-12-31 23:40:03 +0700

    add service layer

--- src/main/java/com/example/latihan2/controller/CobaController.java
+++ src/main/java/com/example/latihan2/controller/CobaController.java
@@ -1,15 +1,21 @@
 package com.example.latihan2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.latihan2.service.CobaService;
+
 @RestController
 public class CobaController {
 	
+	@Autowired
+	CobaService cobaService;
+	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World!")String name) {
-		return String.format("Hello %s", name);
+		return String.format("Hello %s, just changed to %s", name, cobaService.changeNameToBudi());
 	}
 
 }
---
+++ src/main/java/com/example/latihan2/service/CobaService.java
@@ -0,0 +1,13 @@
+package com.example.latihan2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CobaService {
+	
+	public String changeNameToBudi() {
+		return "Budi";
+	}
+	
+
+}

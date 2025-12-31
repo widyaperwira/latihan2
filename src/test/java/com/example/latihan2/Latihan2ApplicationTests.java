d07da030d02e471b39c80cbe54c6736011e0a440
Widya Perwira Utama <widyaperwira@gmail.com>
2025-12-31 21:58:32 +0700

    init

---
+++ src/test/java/com/example/latihan2/Latihan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
